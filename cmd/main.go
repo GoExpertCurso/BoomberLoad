fd2b6c7aaa77a8d3e30b6c38e05115b086d47802
Luciano Santos <luciano_z7@hotmail.com>
2024-04-16 22:31:09 -0300

    Adding requester entity

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/http"
 	"os"
 	"runtime"
 	"time"
+
+	"github.com/GoExpertCurso/BoomerLoad/internal/entity"
 )
 
 var (
@@ -53,7 +54,47 @@ func main() {
 		usageAndExit("-r cannot be less than -c.")
 	}
 
-	done := make(chan bool)
+	work := entity.NewWorker(url, num, conc)
+
+	work.Done = make(chan bool)
+	work.ResultChan = make(chan bool, num)
+	start := time.Now()
+	fmt.Printf("\nStarting load test for %s with %d concurrent requests for %d requests\n ", url, conc, num)
+	for i := 0; i < conc; i++ {
+		go work.Worker()
+	}
+	lista := []entity.RequestDetails{}
+	count200 := 0
+	count400 := 0
+	count500 := 0
+	count429 := 0
+
+	for j := 0; j < num; j++ {
+		fmt.Println("Pegando o resultado")
+		<-work.ResultChan
+		lista = append(lista, *<-work.HttpDetails)
+		//<-work.HttpDetails
+		//fmt.Println(<-work.HttpDetails)
+	}
+
+	for _, v := range lista {
+		fmt.Println("code: ", v.Code)
+		if v.Code == 200 {
+			count200++
+		}
+		if v.Code == 400 {
+			count400++
+		}
+		if v.Code == 500 {
+			count500++
+		}
+		if v.Code == 429 {
+			count429++
+		}
+	}
+
+	work.Close()
+	/* done := make(chan bool)
 	resultChan := make(chan bool, num)
 
 	start := time.Now()
@@ -70,38 +111,8 @@ func main() {
 		<-resultChan
 	}
 
-	/* for i := 0; i < conc; i++ {
-		wg.Add(1)
-		for j := 0; j < reqPerConc; j++ {
-			fmt.Println("i:", i, "j:", j)
-			go func() {
-				defer wg.Done()
-				client := http.Client{}
-				for j := 0; j < reqPerConc; j++ {
-					select {
-					case <-done:
-						return
-					default:
-						req, err := http.NewRequest("GET", url, nil)
-						if err != nil {
-							fmt.Printf("Error creating request: %v", err)
-							continue
-						}
-						res, err := client.Do(req)
-						if err != nil {
-							fmt.Printf("Error making request: %v", err)
-							continue
-						}
-						res.Body.Close()
-					}
-				}
-			}()
-		}
-	} */
 
-	//time.Sleep(time.Second * time.Duration(1))
-
-	close(done)
+	close(done) */
 
 	durationSeconds := time.Since(start).Seconds()
 
@@ -109,6 +120,10 @@ func main() {
 	fmt.Printf("Load test completed in %f seconds\n", durationSeconds)
 	fmt.Printf("Total requests: %d\n", num)
 	fmt.Printf("Requests per second: %2.2f\n", requestsPerSecond)
+	fmt.Printf("200 OK: %d\n", count200)
+	fmt.Printf("400 OK: %d\n", count400)
+	fmt.Printf("500 OK: %d\n", count500)
+	fmt.Printf("429 OK: %d\n", count429)
 }
 
 func usageAndExit(msg string) {
@@ -121,7 +136,7 @@ func usageAndExit(msg string) {
 	os.Exit(1)
 }
 
-func worker(url string, reqPerConc int, done <-chan bool, resultChan chan<- bool) {
+/* func worker(url string, reqPerConc int, done <-chan bool, resultChan chan<- bool) {
 	client := http.Client{}
 	for j := 0; j < reqPerConc; j++ {
 		fmt.Println("Thread: ", j)
@@ -143,4 +158,4 @@ func worker(url string, reqPerConc int, done <-chan bool, resultChan chan<- bool
 			resultChan <- true
 		}
 	}
-}
+}*/
---
+++ internal/entity/requester.go
@@ -0,0 +1,107 @@
+package entity
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type ResponseCounter struct {
+	StatusCodeCounts map[int]int
+}
+
+func NewResponseCounter() *ResponseCounter {
+	return &ResponseCounter{
+		StatusCodeCounts: make(map[int]int),
+	}
+}
+
+func (rc *ResponseCounter) IncrementStatusCodeCount(statusCode int) {
+	rc.StatusCodeCounts[statusCode]++
+}
+
+func (rc *ResponseCounter) PrintStatusCodes() {
+	fmt.Println("Status Code Counts:")
+	for code, count := range rc.StatusCodeCounts {
+		fmt.Printf("%d: %d\n", code, count)
+	}
+}
+
+type RequestDetails struct {
+	Code int
+	Time float64
+}
+
+type Work struct {
+	//Url of request
+	Url string
+
+	//Number of requests to make
+	Requests int
+
+	//Number of concurrent requests to make
+	NumberConcurrent int
+
+	//Channel to signal completion
+	Done chan bool
+
+	//Channel to signal result
+	ResultChan chan bool
+
+	//Channel to signal timeout
+	Timeout chan bool
+
+	//Channel to requests details
+	HttpDetails chan *RequestDetails
+
+	//Total number of requests completed
+	CompletedRequests int
+}
+
+func NewRequestDetails(code int, time float64) *RequestDetails {
+	return &RequestDetails{
+		Code: code,
+		Time: time,
+	}
+}
+
+func NewWorker(url string, requests, numberConcurrent int) *Work {
+	return &Work{
+		Url:              url,
+		Requests:         requests,
+		NumberConcurrent: numberConcurrent,
+		Done:             make(chan bool),
+		ResultChan:       make(chan bool, requests),
+		Timeout:          make(chan bool),
+		HttpDetails:      make(chan *RequestDetails, requests),
+	}
+}
+
+func (w *Work) Worker() {
+	client := http.Client{}
+	for j := 0; j < w.Requests/w.NumberConcurrent; j++ {
+		fmt.Println("Thread: ", j)
+		select {
+		case <-w.Done:
+			return
+		default:
+			req, err := http.NewRequest("Post", w.Url, nil)
+			if err != nil {
+				fmt.Printf("Error creating request: %v", err)
+				continue
+			}
+			res, err := client.Do(req)
+			w.HttpDetails <- NewRequestDetails(res.StatusCode, 1.0)
+			if err != nil {
+				fmt.Printf("Error making request: %v", err)
+				continue
+			}
+			//fmt.Println(res.Body)
+			res.Body.Close()
+			w.ResultChan <- true
+		}
+	}
+}
+
+func (w *Work) Close() {
+	close(w.Done)
+}
